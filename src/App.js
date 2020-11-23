f66bd1b58921215ce6e4f2ee121dbc372a80d890
matteo-lovato <lovato.teo@gmail.com>
2020-11-23 23:04:41 +0100

    global styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import GlobalStyle from "./components/GlobalStyles";
 import Home from "./pages/Home";
 
 function App() {
     return (
         <div className="App">
+            <GlobalStyle />
             <Home />
         </div>
     );
