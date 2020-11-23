434459e694f6686ceff40df8e2d48c7a474010b6
matteo-lovato <lovato.teo@gmail.com>
2020-11-23 21:54:44 +0100

    fetching new and upcoming games

--- src/actions/gameAction.js
+++ src/actions/gameAction.js
@@ -1,15 +1,19 @@
 import axios from "axios";
-import { popularGamesURL } from "../api";
+import { newGamesURL, popularGamesURL, upcomingGamesURL } from "../api";
 
 //action creator
 
 export const loadGames = () => async (dispatch) => {
     // fetch axios
     const popularData = await axios.get(popularGamesURL());
+    const upcomingData = await axios.get(upcomingGamesURL());
+    const newGamesData = await axios.get(newGamesURL());
     dispatch({
         type: "FETCH_GAMES",
         payload: {
             popular: popularData.data.results,
+            upcoming: upcomingData.data.results,
+            newGames: newGamesData.data.results,
         },
     });
 };
--- src/reducers/gamesReducer.js
+++ src/reducers/gamesReducer.js
@@ -1,16 +1,21 @@
 const initState = {
-  popular: [],
-  newGames: [],
-  upcoming: [],
-}
+    popular: [],
+    newGames: [],
+    upcoming: [],
+};
 
 const gamesReducer = (state = initState, action) => {
-  switch (action.type) {
-    case "FETCH_GAMES":
-      return { ...state, popular: action.payload.popular }
-    default:
-      return { ...state }
-  }
-}
+    switch (action.type) {
+        case "FETCH_GAMES":
+            return {
+                ...state,
+                popular: action.payload.popular,
+                upcoming: action.payload.upcoming,
+                newGames: action.payload.newGames,
+            };
+        default:
+            return { ...state };
+    }
+};
 
-export default gamesReducer
+export default gamesReducer;
