5baba67b37177a0fc86901b0df4cfa0745d9e350
matteo-lovato <lovato.teo@gmail.com>
2020-11-24 15:42:49 +0100

    stars review

--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -13,6 +13,9 @@ import xbox from "../img/xbox.svg";
 import nintendo from "../img/nintendo.svg";
 import apple from "../img/apple.svg";
 import gamepad from "../img/gamepad.svg";
+// star images
+import starEmpty from "../img/star-empty.png";
+import starFull from "../img/star-full.png";
 
 const GameDetail = ({ gameId }) => {
     const history = useHistory();
@@ -41,6 +44,20 @@ const GameDetail = ({ gameId }) => {
         }
     };
 
+    // rating logic
+    const getStars = () => {
+        const stars = [];
+        const rating = Math.floor(game.rating);
+        for (let i = 1; i <= 5; i++) {
+            if (i <= rating) {
+                stars.push(<img alt="star" key={i} src={starFull} />);
+            } else {
+                stars.push(<img alt="star" key={i} src={starEmpty} />);
+            }
+        }
+        return stars;
+    };
+
     const { screen, game, isLoading } = useSelector(state => state.detail);
     return (
         <>
@@ -52,7 +69,7 @@ const GameDetail = ({ gameId }) => {
                                 <motion.h3 layoutId={`title ${gameId}`}>
                                     {game.name}
                                 </motion.h3>
-                                <p>Rating: {game.rating}</p>
+                                <p>Rating: {getStars()}</p>
                             </div>
                             <Info>
                                 <h3>Platforms</h3>
@@ -133,6 +150,11 @@ const Stats = styled(motion.div)`
     display: flex;
     align-items: center;
     justify-content: space-between;
+    img {
+        width: 2rem;
+        height: 2rem;
+        display: inline;
+    }
 `;
 
 const Info = styled(motion.div)`
